be2b7352179adbf6eef4d4322f2984abf378a790
millstone <soyotime0118@gmail.com>
2022-11-02 15:08:53 +0900

    project setup

---
+++ src/main/java/jpabook/jpashop/JpaExPart1Application.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaExPart1Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JpaExPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/jpashop/JpaExPart1ApplicationTests.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaExPart1ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
