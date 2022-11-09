699a024e560e1d2d4049e3b979d63438d04477e9
millstone <soyotime0118@gmail.com>
2022-11-09 21:18:03 +0900

    주문 검색 동적쿼리

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,9 +3,13 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 
+import java.util.List;
+
 public interface OrderRepository
 {
     void save( Order order);
 
     Order findById(Long id);
+
+    List<Order> findAll(OrderSearch orderSearch);
 }
--- src/main/java/jpabook/jpashop/repository/OrderRepositoryImpl.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepositoryImpl.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,6 +23,14 @@ public class OrderRepositoryImpl implements OrderRepository
     public Order findById(Long id)
     {
         return entityManager.find(Order.class, id);
+    }
+
+    @Override
+    public List<Order> findAll(OrderSearch orderSearch)
+    {
+        return entityManager.createQuery("select o from Order o join o.member", Order.class)
+
+                .getResultList();
     }
 
 }
